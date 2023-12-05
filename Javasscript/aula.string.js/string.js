5cbf5fa5e5672200530c1a392f2e8ce9c92dd5ee
NetoFreitass <143613993+NetoFreitass@users.noreply.github.com>
2023-12-05 12:13:24 -0300

    Add files via upload

---
+++ Javasscript/aula.string.js/string.js
@@ -0,0 +1,6 @@
+
+//declaração das variáveis
+
+    //Recebendo valores do cliente
+    
+    //Exibindo produto do cliente
---
+++ Javasscript/lista.de.itens/main.js
@@ -0,0 +1,29 @@
+//Variáveis dos elementos da lista
+
+//Atribuindo o formulário a uma variável
+const form = document.getElementById('addForm')
+//Atribuir a lista a uma variável 
+const itemlist = document.getElementById('Items')
+//Atribuição da barra de pesquisa 
+const filter = document.getElementById('filter')
+
+form.addEventListener('submit', additem);
+itemlist.addEventListener('click',removeItem);
+filter.addEventListener('keyup', buscaritems);
+
+function buscaritems() {
+    alert ('tecla ativa');
+}
+
+function removeItem() {
+    alert ('clicou na UL');
+}
+
+function additem() {
+    /* alert('você clicou') */
+}
+
+function additem(evento) {
+    evento.preventdefault();
+    console.log(evento)
+}
